cd77f2a2c47f6ae1357440c3a01f4406f50415c1
Alan Rodrigues Soares <alanrsoars@gmail.com>
2013-12-09 01:45:53 -0200

    Failing tests

--- src/Pingdom.Client.Tests/PingdomClientResourcesTests.cs
+++ src/Pingdom.Client.Tests/PingdomClientResourcesTests.cs
@@ -55,5 +55,37 @@ namespace Pingdom.Client.Tests
             Assert.IsNotNull(deleteCheckReponse);
             Assert.IsFalse(deleteCheckReponse.HasErrors);
         }
+
+        [Test]
+        public async Task FullCRUDFailingTest()
+        {
+            //create
+            var newCheck = new { invalidparameter = "" };
+            var createNewCheckResponse = await Pingdom.Client.Checks.CreateNewCheck(newCheck);
+            Assert.IsNotNull(createNewCheckResponse);
+            Assert.IsTrue(createNewCheckResponse.HasErrors);
+
+            ////retrieve
+            //Contracts.GetDetailedCheckInformationResponse retrievedCheckResponse = await Pingdom.Client.Checks.GetDetailedCheckInformation(createNewCheckResponse.Check.Id);
+            //Assert.IsNotNull(retrievedCheckResponse);
+            //Assert.IsFalse(retrievedCheckResponse.HasErrors);
+            //Assert.IsNotNull(retrievedCheckResponse.Check);
+
+            ////update
+            //dynamic checkUpdate = new { name = "MyNewCheck-TEST-UPDATE", host = "www.my-updated-test-host-check.com.br" };
+            //var updateCheckResponse = await Pingdom.Client.Checks.ModifyCheck(retrievedCheckResponse.Check.Id, checkUpdate);
+            //Assert.IsNotNull(updateCheckResponse);
+            //Assert.IsFalse(updateCheckResponse.HasErrors);
+
+            ////confirm
+            //retrievedCheckResponse = await Pingdom.Client.Checks.GetDetailedCheckInformation(retrievedCheckResponse.Check.Id);
+            //Assert.AreEqual(checkUpdate.name, retrievedCheckResponse.Check.Name);
+            //Assert.AreEqual(checkUpdate.host, retrievedCheckResponse.Check.HostName);
+
+            ////delete
+            //var deleteCheckReponse = await Pingdom.Client.Checks.DeleteCheck(retrievedCheckResponse.Check.Id);
+            //Assert.IsNotNull(deleteCheckReponse);
+            //Assert.IsFalse(deleteCheckReponse.HasErrors);
+        }
     }
 }
