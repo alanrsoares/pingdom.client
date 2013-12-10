d720ba41af5da8696a91a7580bf08b30ffb07ef2
Renan Porto <renanporto92@yahoo.com.br>
2013-12-10 14:30:17 -0200

    Implements queryString builder, creates new tests for Actions resource

--- src/Pingdom.Client/Contracts/TraceRoute.cs
+++ src/Pingdom.Client/Contracts/TraceRoute.cs
@@ -2,8 +2,19 @@ namespace Pingdom.Client.Contracts
 {
     public class TraceRoute
     {
+        /// <summary>
+        /// Traceroute output
+        /// </summary>
         public string Result { get; set; }
+
+        /// <summary>
+        /// Probe identifier
+        /// </summary>
         public int ProbeId { get; set; }
+
+        /// <summary>
+        /// Probe description
+        /// </summary>
         public string ProbeDescription { get; set; }
     }
 }
